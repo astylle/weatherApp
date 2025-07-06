a3d2af845f82f2339506236860dff50decd8c1b1
user_hc <100005741+astylle@users.noreply.github.com>
2025-07-06 17:54:51 +0200

    saving search information to local storage for easier future access

--- src/weather/fetchingAPI.js
+++ src/weather/fetchingAPI.js
@@ -1,5 +1,7 @@
 ///User input information 
-let myKey = "A7LTJY4SMSV4V2JNWLW8NXDXQ"
+let apiKey = "A7LTJY4SMSV4V2JNWLW8NXDXQ"
+const key = "weatherData"
+let storedArray = JSON.parse(localStorage.getItem(key)) || [];
 
 export function locationSearch(e){
     e.preventDefault()
@@ -8,18 +10,46 @@ export function locationSearch(e){
     grabLocationData(userInput)
 } 
 
-// function WeatherReport(temp,date,)
+// class WeatherReport{
+//     constructor(
+
+//     )
+//     // fukill this in with information from the function below, but using objects inside objects, meaning that it would be *currentConditions and the *days[0] categories
+// }
+
+function WeatherReport(temp,feelsLike,date,desc,sunrise,sunset){
+    this.temp = temp;
+    this.feelsLike = feelsLike;
+    this.date = date;
+    this.desc = desc;
+    this.sunrise = sunrise;
+    this.sunset = sunset;
+}
 
 async function grabLocationData(query){
     try{
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}/next5days?unitGroup=metric&key=${myKey}`)
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}/next5days?unitGroup=metric&key=${apiKey}`)
         if(!response.ok){
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json() // puts the information in json
         console.log("Results: ", data)
+        console.log("temp", data.currentConditions.feelslike)
+        console.log("days", data.days[0].datetime)
+        storeWeatherData(data.currentConditions.temp, data.currentConditions.feelsLike, data.days[0].datetime, data.days[0].description, data.currentConditions.sunrise, data.currentConditions.sunset)
     } catch (error) {
         console.error("Fetch Error: ", error)
     }
 }
 
+function storeWeatherData(temp, feelslike, date, desc,sunrise,sunset){
+    //creates and stores in local storage
+    let currentWeather = new WeatherReport(temp, feelslike, date, desc, sunrise, sunset)
+    console.log(currentWeather)
+    storedArray.push(currentWeather)
+    console.log("the stored array is:" + storedArray)
+
+    localStorage.setItem(key, JSON.stringify(storedArray))
+    console.log("Local Storage" + localStorage)
+
+}   
